d43f187b330c43a642af87e1f32c3903a2ccd245
Eryk Warchulski <fryd.wtf@gmail.com>
2020-09-30 08:15:49 +0200

    Update eval-utils.cpp

--- src/eval-utils.cpp
+++ src/eval-utils.cpp
@@ -9,10 +9,8 @@ auto eval_point(const des::Point& px, std::function<double(des::Point)> func) ->
 
 auto eval_pop(const des::Population& pop, std::function<double(des::Point)> func) -> std::vector<double> {
 	std::vector<double> fitness;
-
 	for (int i = 0; i < pop.cols(); ++i) 
 		fitness.push_back(func(pop.col(i)));
-
 	return fitness;	
 }
 
